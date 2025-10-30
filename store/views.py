f5aae2764e5cf30be2c9aa751701110d9cb57367
adhikari-ishan <adhikariishan02@gmail.com>
2025-10-30 21:06:34 +0545

    coding...

--- store/views.py
+++ store/views.py
@@ -5,8 +5,31 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms 
-from .forms import SignUpForm, UpdateUserForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
 # Create your views here.
+
+
+def update_password(request):
+    if request.user.is_authenticated:
+        current_user = request.user
+        if request.method == 'POST':
+            form = ChangePasswordForm(current_user,request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request,"Your Password has been updated ")
+                login(request, current_user)
+                return redirect('update_user')
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request, error)
+                    return redirect('update_password')
+        else:
+            form = ChangePasswordForm(current_user)
+            return render(request,"update_password.html", { 'form': form })
+    else:
+        messages.success(request,"You must be logged in")
+        return redirect('home')
+
 
 def update_user(request):
     if request.user.is_authenticated:
